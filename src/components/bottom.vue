237dcb376652cb6329625894ff9e39c77f20b390
hkwhg <592871458@qq.com>
2025-07-02 14:21:39 +0800

    fix bottom styles

--- src/components/bottom.vue
+++ src/components/bottom.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="footer__container">
+    <h2 class="footer__title">HUBUNGI THE THIRD OCEAN TOWER</h2>
     <div class="footer__context">
       <div class="footer__item">
-        <h2>THE THIRD OCEAN TOWER</h2>
         <p>
           At THE THIRD OCEAN TOWER, we specialize in providing top-notch IT services to help
           you tackle challenges with your digital devices and software. Whether
@@ -12,8 +12,7 @@
           committed to enhancing your digital experience.
         </p>
       </div>
-      <div class="footer__item">
-        <h2>HUBUNGI THE THIRD OCEAN TOWER</h2>
+      <div class="footer__item item_contact">
         <router-link to="/faq">FAQ</router-link>
         <router-link to="/layanan">Service</router-link>
         <router-link to="/kontak">Contact</router-link>
@@ -22,7 +21,6 @@
         <router-link to="/refund">Refund policy</router-link>
       </div>
       <div class="footer__item">
-        <h2>HUBUNGI THE THIRD OCEAN TOWER</h2>
         <h4>dpsuppkr@gmail.com</h4>
         <h4>+92 03458744788</h4>
         <h4>The Third Ocean Tower Private Limited Office No 30 Floor 3 Anayat Mall Plaza G11 Markaz Islamabad</h4>
@@ -34,12 +32,21 @@
 <style lang="scss" scoped>
 .footer__container {
   width: 100%;
+  padding: 106px 0;
   background-color: #ffffff;
+
+  .footer__title {
+    text-align: center;
+    color: #000000;
+    font-size: 32px;
+    font-weight: 800;
+    margin-bottom: 50px;
+  }
 
   .footer__context {
     width: 1215px;
     margin: 0 auto;
-    padding: 106px 40px;
+    padding: 0 40px;
     display: flex;
     justify-content: space-between;
     align-items: flex-start;
@@ -56,17 +63,17 @@
     }
 
     h4 {
-      font-size: 20px;
-      font-weight: 600;
+      font-size: 16px;
       color: #000000;
       margin-bottom: 10px;
+      font-weight: normal;
+      line-height: 22px;
     }
 
     a {
       display: block;
       color: #000000;
       font-size: 16px;
-      font-weight: 600;
       margin-bottom: 10px;
       cursor: pointer;
     }
@@ -78,6 +85,11 @@
       font-weight: 400;
       line-height: 24px;
     }
+  }
+
+  .item_contact {
+    padding-left: 100px;
+    box-sizing: border-box;
   }
 }
 </style>
